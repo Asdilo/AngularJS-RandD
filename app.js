d1a244652e50d816d02ce2da213c05039d763ab9
Asterisk Loftis <asterisk@asteriskloftis.com>
2015-07-01 15:26:48 -0600

    Made the project generic and removed the img folder. I need the r and d project as lean as possible.

--- app.js
+++ app.js
@@ -1,36 +1,7 @@
 (function () {
-	var app = angular.module('store', []);
+	var app = angular.module('randd', []);
 
-	app.controller('StoreController', function () {
-		this.products = gems;
+	app.controller('RanddController', function () {
+
 	});
-
-	var gems = [
-		{
-			name: 'Dodecahedron',
-			price: 2.95,
-			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas aliquam rutrum.',
-			images: [
-				{
-					full: 'img/rabobank-logo.jpg',
-					thumb: 'img/rabobank-logo68x80.jpg'
-				}
-			],
-			canPurchase: false,
-			soldOut: false,
-		},
-		{
-			name: 'Asterisk Gem',
-			price: 312.84,
-			description: 'The eight sided most ballin-est gem evaaars',
-			images: [
-				{
-					full: 'img/rabobank-logo.jpg',
-					thumb: 'img/rabobank-logo68x80.jpg'
-				}
-			],
-			canPurchase: true,
-			soldOut: false,
-		}
-	];
-})();
+});
